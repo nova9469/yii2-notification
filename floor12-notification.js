c3ac138b9afac6830cf4511df1237d3bb1c02c1d
nova9469 <32396623+nova9469@users.noreply.github.com>
2021-11-30 19:30:21 +0200

    Merge remote-tracking branch 'origin/dev-nova9469' into dev-nova9469

--- floor12-notification.js
+++ floor12-notification.js
@@ -9,15 +9,12 @@ f12notification = {
 
         if (type == 1) {
             info_class = 'info-object-success';
-            timeout = 3000;
             svg = '<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"/></svg>'
-
         }
+
         if (type == 2) {
             info_class = 'info-object-error';
-            timeout = 3000;
             svg = '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-circle" class="svg-inline--fa fa-exclamation-circle fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"/></svg>';
-
         }
 
         const info = document.createElement('div');

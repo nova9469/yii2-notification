fbe3fad8a5f15f66bc919fa355258424a10ee591
nova9469 <32396623+nova9469@users.noreply.github.com>
2021-11-30 17:59:53 +0200

    - added timeout regulation + css

--- floor12-notification.js
+++ floor12-notification.js
@@ -1,10 +1,9 @@
 f12notification = {
     infoListBlock: document.createElement('div'),
 
-    notification: function (content, type) {
+    notification: function (content, type, timeout = 3000) {
         f12notification.initNotificationBlock();
 
-        timeout = 3000
         svg = '<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"/></svg>'
         info_class = 'info-object-info';
 
@@ -36,14 +35,14 @@ f12notification = {
         }, timeout * 2);
     },
 
-    success: function (content) {
-        this.notification(content, 1)
+    success: function (content, timeout = 3000) {
+        this.notification(content, 1, timeout)
     },
-    error: function (content) {
-        this.notification(content, 2)
+    error: function (content, timeout = 3000) {
+        this.notification(content, 2, timeout)
     },
-    info: function (content) {
-        this.notification(content, 0)
+    info: function (content, timeout = 3000) {
+        this.notification(content, 0, timeout)
     },
 
     initNotificationBlock: function () {
@@ -54,9 +53,9 @@ f12notification = {
     }
 }
 
-function info(content, type) {
+function info(content, type, timeout = 3000) {
     console.warn('Function info() is deprecated. Please, replace info() for f12notification.info(), f12notification.success() or f12notification.error().')
-    return f12notification.notification(content, type);
+    return f12notification.notification(content, type, timeout);
 }
 
 
